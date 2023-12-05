c7d2f0ffaf8c048fefb8ba08846973e89138721d
unknown <sup.makulin@mail.ru>
2023-12-06 00:23:05 +0300

    homplete hw6

--- JS_continue/hw6/items.js
+++ JS_continue/hw6/items.js
@@ -1,37 +1,32 @@
-const itemsData = `[{
-        "img": "./img/item1.jpg",
-        "title": "ELLERY X M'O CAPSULE",
-        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "price": "$52.00"
-    },
-    {
-        "img": "./img/item2.jpg",
-        "title": "ELLERY X M'O CAPSULE",
-        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "price": "$52.00"
-    },
-    {
-        "img": "./img/item3.jpg",
-        "title": "ELLERY X M'O CAPSULE",
-        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "price": "$52.00"
-    },
-    {
-        "img": "./img/item4.jpg",
-        "title": "ELLERY X M'O CAPSULE",
-        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "price": "$52.00"
-    },
-    {
-        "img": "./img/item5.jpg",
-        "title": "ELLERY X M'O CAPSULE",
-        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "price": "$52.00"
-    },
-    {
-        "img": "./img/item6.jpg",
-        "title": "ELLERY X M'O CAPSULE",
-        "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
-        "price": "$52.00"
-    }
+"use strict";
+const itemsData = `[{"img": "./img/item1.jpg",
+"title": "ELLERY X MO CAPSULE",
+"text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+"price": "$52.00"
+}, {
+    "img": "./img/item2.jpg",
+    "title": "ELLERY X M'O CAPSULE",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+}, {
+    "img": "./img/item3.jpg",
+    "title": "ELLERY X M'O CAPSULE",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+}, {
+    "img": "./img/item4.jpg",
+    "title": "ELLERY X M'O CAPSULE",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+}, {
+    "img": "./img/item5.jpg",
+    "title": "ELLERY X M'O CAPSULE",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+}, {
+    "img": "./img/item6.jpg",
+    "title": "ELLERY X M'O CAPSULE",
+    "text": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+    "price": "$52.00"
+}
 ]`;
--- JS_continue/hw6/script.js
+++ JS_continue/hw6/script.js
@@ -1,5 +1,18 @@
+"use strict";
+
 const containerEl = document.querySelector(".container");
-console.log(JSON.parse(itemsData));
+const containerTitleEl = document.createElement('h2');
+const containerSubTitleEl = document.createElement('p');
+const itemsArr = JSON.parse(itemsData);
+
+containerTitleEl.innerHTML = "Fetured Items";
+containerSubTitleEl.innerHTML = "Shop for items based on what we featured in this week";
+
+containerTitleEl.classList.add('container_title');
+containerSubTitleEl.classList.add('container_subTitle');
+
+containerEl.appendChild(containerTitleEl);
+containerEl.appendChild(containerSubTitleEl);
 
 itemsArr.forEach(element => {
     const imgEl = document.createElement("img");
@@ -14,11 +27,16 @@ itemsArr.forEach(element => {
     textEl.innerHTML = element.text;
     priceEl.innerHTML = element.price;
 
+    elementDiv.classList.add('container__item');
+    imgEl.classList.add('container__item_img');
+    titleEl.classList.add('container__item_title');
+    textEl.classList.add('container__item_text');
+    priceEl.classList.add('container__item_price');
+
     elementDiv.appendChild(imgEl);
     elementDiv.appendChild(titleEl);
     elementDiv.appendChild(textEl);
     elementDiv.appendChild(priceEl);
 
     containerEl.appendChild(elementDiv);
-
 });
