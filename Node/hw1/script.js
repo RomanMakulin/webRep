61149bfae71308d13303c30a0134d1e2ecad1973
unknown <sup.makulin@mail.ru>
2024-01-09 21:49:33 +0300

    compl hw1 node js

--- Node/hw1/script.js
+++ Node/hw1/script.js
@@ -1,21 +1,30 @@
 const http = require('http');
 
+let countMain = 0;
+let countAbout = 0;
+
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.writeHead(200, {
             'Content-Type': "text/html; charset=UTF-8"
         });
-        res.end('<h1>Hello na /</h1>');
-    } else if (req.url === '/main') {
+        countMain++;
+        res.end(`<h1>Main page</h1>\n
+        <p>clicked: [${countMain}]</p>\n
+        <a href="/about">about link</a>`);
+    } else if (req.url === '/about') {
         res.writeHead(200, {
             'Content-Type': "text/html; charset=UTF-8"
         });
-        res.end('<h1>Hello na main </h1>');
+        countAbout++;
+        res.end(`<h1>About page</h1>\n
+        <p>clicked: [${countAbout}]</p>\n
+        <a href="/">main link</a>`);
     } else {
         res.writeHead(400, {
             'Content-Type': "text/html; charset=UTF-8"
         });
-        res.end('<h1>Not found</h1>');
+        res.end('<h1>Not found page (404)</h1>');
     }
 });
 
